bf20ea71633e9e6ec27263d2b2257d7165237564
alvesxdani <dani.alvesm22@gmail.com>
2022-03-22 18:10:33 -0300

    Add more contents (box, login, navbar)

---
+++ assets/scripts.js
@@ -0,0 +1,13 @@
+// PAGE STUDENT
+function pageStudent() {
+    document.getElementById('iframe').innerHTML = document.getElementById("pageStudent").innerHTML;
+    document.getElementById('iframe').style.marginTop = "100px";
+    document.getElementById('iframe').style.transition = "ease-in 0.9s";
+}
+
+// PAGE TEACHER
+function pageTeacher() {
+    document.getElementById('iframe').innerHTML = document.getElementById("pageTeacher").innerHTML;
+    document.getElementById('iframe').style.marginTop = "100px";
+    document.getElementById('iframe').style.transition = "ease-in 0.9s";
+}
