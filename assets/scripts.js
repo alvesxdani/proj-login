83970150b9815debd06b9386f8c25327475ba00c
alvesxdani <dani.alvesm22@gmail.com>
2022-03-23 00:06:54 -0300

    Add some contents

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,3 +11,12 @@ function pageTeacher() {
     document.getElementById('iframe').style.marginTop = "100px";
     document.getElementById('iframe').style.transition = "ease-in 0.9s";
 }
+
+// FORGOT PASSWORD PAGE
+function forgotPass() {
+    document.getElementById('pageStudent').innerHTML = document.getElementById('forgotPass').innerHTML;
+    document.getElementById("pageTeacher").innerHTML = document.getElementById("forgotPass").innerHTML;
+    document.getElementById("iframe").innerHTML = document.getElementById('forgotPass').innerHTML;
+    document.getElementById('iframe').style.marginTop = "100px";
+    document.getElementById('iframe').style.transition = "ease-in 0.9s";
+}
