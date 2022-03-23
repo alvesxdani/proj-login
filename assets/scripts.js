7a97a27772521317019cb723094b82ecc44abbed
alvesxdani <dani.alvesm22@gmail.com>
2022-03-23 18:11:14 -0300

    Add some stuff for finish

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,22 +1,16 @@
 // PAGE STUDENT
 function pageStudent() {
-    document.getElementById('iframe').innerHTML = document.getElementById("pageStudent").innerHTML;
+    document.getElementById('iframe').innerHTML = document.getElementById('pageStudent').innerHTML;
     document.getElementById('iframe').style.marginTop = "100px";
     document.getElementById('iframe').style.transition = "ease-in 0.9s";
 }
 
 // PAGE TEACHER
 function pageTeacher() {
-    document.getElementById('iframe').innerHTML = document.getElementById("pageTeacher").innerHTML;
+    document.getElementById('iframe').innerHTML = document.getElementById('pageTeacher').innerHTML;
     document.getElementById('iframe').style.marginTop = "100px";
     document.getElementById('iframe').style.transition = "ease-in 0.9s";
 }
 
-// FORGOT PASSWORD PAGE
-function forgotPass() {
-    document.getElementById('pageStudent').innerHTML = document.getElementById('forgotPass').innerHTML;
-    document.getElementById("pageTeacher").innerHTML = document.getElementById("forgotPass").innerHTML;
-    document.getElementById("iframe").innerHTML = document.getElementById('forgotPass').innerHTML;
-    document.getElementById('iframe').style.marginTop = "100px";
-    document.getElementById('iframe').style.transition = "ease-in 0.9s";
-}
+// MARGIN FOR BOX ON FORGOTPASS.HTML
+document.getElementById('forgotPassw').style.marginTop = "100px";
